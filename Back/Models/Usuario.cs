10c8607ceb22dfd52dcbf2382ac7f41bde59cec7
MarlonRoches <petozza123>
2019-11-24 20:29:11 -0600

    Vistas: Login y Create implementadas SDES implementado singleton Implementado Core implementado Clases implementadas

---
+++ Back/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Password { get; set; }
+
+    }
+}
