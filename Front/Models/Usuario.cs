4d8c3f12cab2660ebc06583a438b3bd7561949a2
MarlonRoches <roches856@gmail.com>
2019-11-27 12:39:15 -0600

    Actualizar

--- Front/Models/Usuario.cs
+++ Front/Models/Usuario.cs
@@ -7,11 +7,11 @@ namespace Back.Models
 {
     public class Usuario
     {
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string User { get; set; }
-        public string eMail { get; set; }
-        public string Password { get; set; }
-        public int LlaveSDES { get; set;}
+        public string Nombre    {get; set; }
+        public string Apellido  { get; set; }
+        public string User      { get; set; }
+        public string eMail     { get; set; }
+        public string Password  { get; set; }
+        public int    LlaveSDES { get; set;}
     }
 }
