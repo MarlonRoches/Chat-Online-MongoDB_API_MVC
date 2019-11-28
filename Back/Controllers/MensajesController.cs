084bfb868e04b0d318e4190f7abfb9a5ea319eb7
marcelo rosales <mgrt99@gmail.com>
2019-11-28 15:22:33 -0600

    Metodos put y post mensajes

--- Back/Controllers/MensajesController.cs
+++ Back/Controllers/MensajesController.cs
@@ -38,7 +38,7 @@ namespace Back.Controllers
             }
         }
         [HttpPost]
-        [Route ("CrearConversacion")]
+        [Route ("CrearConversacionEmisor")]
         public IActionResult post([FromBody] Mensaje _nuevo)
         {
             if (ModelState.IsValid)
@@ -55,13 +55,123 @@ namespace Back.Controllers
                     CuentaController ModificarContactos = new CuentaController(nuevo2);
                     ModificarContactos.ModificarContactos(_nuevo.Emisor, _nuevo.Receptor);
                 }
+             
               
 
 
             }
             return NoContent();
         }
+        [HttpPost]
+        [Route ("CrearConversacionReceptor")]
+        public IActionResult PostReceptor([FromBody] Receptor _nuevo)
+        {
+            if (ModelState.IsValid)
+            {
+                var modelo = _mensajes.Get(_nuevo.IDEmisorReceptor);
+                if (modelo == null)
+                {
+                    Mensaje nuevo = new Mensaje();
+                    Extesiones AgregarTexto = new Extesiones();
+                    Dictionary<DateTime, Extesiones> Emisor = new Dictionary<DateTime, Extesiones>();
+                    Dictionary<DateTime, bool> Indice = new Dictionary<DateTime, bool>();
+                    Indice.Add(_nuevo.HoraMensaje, _nuevo.Origen);
+                    nuevo.IDEmisorReceptor = _nuevo.IDEmisorReceptor;
+                    nuevo.Receptor = _nuevo.Recept;
+                    nuevo.Emisor = _nuevo.Emisor;
+                    AgregarTexto.Texto = _nuevo.Texto;
+                    AgregarTexto.Extesion = _nuevo.Extension;
+                    Emisor.Add(_nuevo.HoraMensaje, AgregarTexto);
+                    nuevo.EmisorMen = Emisor;
+                    _mensajes.Create(nuevo);
+                    UsuariosDatabaseSettings Coneccion = new UsuariosDatabaseSettings();
+                    Coneccion.ConnectionString = "mongodb://localhost:27017";
+                    Coneccion.DatabaseName = "Teules";
+                    Coneccion.UsuariosCollectionName = "usuarios";
+                    UsuarioServicios nuevo2 = new UsuarioServicios(Coneccion);
+                    CuentaController ModificarContactos = new CuentaController(nuevo2);
+                    ModificarContactos.ModificarContactos(_nuevo.Emisor, nuevo.Receptor);
+                }
+                else
+                {
+
+                    //de lo contrario existe por lo tanto agregar el mensaje nuevo al put
+                }
+
+
+
+            }
+            return NoContent();
+        }
+        [HttpPut]
+        [Route ("AgregarMensajeEmisor/{UsuarioCompuesto}")]
+        public IActionResult MensajeNuevo(string UsuarioCompuesto, [FromBody] Mensaje nuevo)
+        {
+            if(ModelState.IsValid)
+            {
+                
+                var modelo = _mensajes.Get(UsuarioCompuesto);
+                if(modelo != null)
+                {
+                    modelo.EmisorMen = nuevo.EmisorMen;
+                    modelo.ReceptorMen = nuevo.ReceptorMen;
+
+                    _mensajes.Update(modelo.Id, modelo);
+                }
+            }
+            return NoContent();
+        }
+        [HttpPut]
+        [Route("AgregarMensajeReceptor/{UsuarioCompuesto}")]
+        public IActionResult MensajeNuevo(string UsuarioCompuesto, [FromBody] Receptor _nuevo)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                var modelo = _mensajes.Get(UsuarioCompuesto);
+                if (modelo != null)
+                {
+                   
+                    Extesiones AgregarTexto = new Extesiones();
+                    Dictionary<DateTime, Extesiones> Emisor = new Dictionary<DateTime, Extesiones>();
+                    Dictionary<DateTime, bool> Indice = new Dictionary<DateTime, bool>();
+                    AgregarTexto.Texto = _nuevo.Texto;
+                    AgregarTexto.Extesion = _nuevo.Extension;
+                    modelo.Receptor = _nuevo.Recept;
+                    modelo.Emisor = _nuevo.Emisor;
+                    modelo.EmisorMen = Emisor;
+
+
+                    if (modelo.EmisorMen != null)
+                    {
+                        Emisor = modelo.EmisorMen;
+                        Indice = modelo.MensajesOrdenados;
+                        Emisor.Add(_nuevo.HoraMensaje, AgregarTexto);
+                        Indice.Add(_nuevo.HoraMensaje, _nuevo.Origen);
+                        modelo.EmisorMen = Emisor;
+                        modelo.MensajesOrdenados = Indice;
+
+                    }
+                    else
+                    {
+                        Emisor.Add(_nuevo.HoraMensaje, AgregarTexto);
+                        Indice.Add(_nuevo.HoraMensaje, _nuevo.Origen);
+                        modelo.EmisorMen = Emisor;
+                        modelo.MensajesOrdenados = Indice;
+
+                     
+                    }
+                   
+                 
+                    _mensajes.Update(modelo.Id, modelo);
+                }
+            }
         
+                    return NoContent();
+
+        }
+
 
 
 
