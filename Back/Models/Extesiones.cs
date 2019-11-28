06c77942fb6e9e78ef3ced65c5cc6514c989c19c
marcelo rosales <mgrt99@gmail.com>
2019-11-27 19:46:59 -0600

    Añadir clase extesiones

---
+++ Back/Models/Extesiones.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back.Models
+{
+    public class Extesiones
+    {
+        public string Extesion { get; set; }
+        public string Texto { get; set; }
+    }
+}
--- Back/Models/Mensaje.cs
+++ Back/Models/Mensaje.cs
@@ -11,8 +11,9 @@ namespace Back.Models
         public string                       Id                  { get; set; }
         [BsonElement("Mensaje")]
         public string                       IDEmisorReceptor    { get; set; }
-        public Dictionary<DateTime, string> Mensjaes            { get; set; }
-        public Dictionary<DateTime, string> Archivos            { get; set;}
+        public Dictionary<DateTime, Extesiones> EmisorMen = new Dictionary<DateTime, Extesiones>();
+        public Dictionary<DateTime, Extesiones> ReceptorMen = new Dictionary<DateTime, Extesiones>();  
+
         public string                       Emisor              { get; set; }
         public string                       Receptor            { get; set; }
         
