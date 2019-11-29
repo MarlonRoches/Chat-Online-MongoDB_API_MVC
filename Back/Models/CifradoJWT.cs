f49d0ee8752b04e0e4d85da67aa840584de7603a
marcelo rosales <mgrt99@gmail.com>
2019-11-29 16:06:40 -0600

    Agregada clase JWT

---
+++ Back/Models/CifradoJWT.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back.Models
+{
+    public class CifradoJWT
+    {
+        public string Nombre { get; set; }
+        public string GenerateToken(string key, CifradoJWT json)
+        {
+
+
+            var llaveSeguridad = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var crencial = new SigningCredentials(llaveSeguridad, SecurityAlgorithms.HmacSha256);
+            var reclamo = new[]
+            {
+                new Claim("usuario",json.Nombre)
+
+            };
+
+
+
+            var token = new JwtSecurityToken("xyz",
+                "xtz", reclamo,
+                DateTime.UtcNow,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: crencial);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+
+        }
+    }
+}
